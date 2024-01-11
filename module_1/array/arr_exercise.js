418ed48093563bd06bf39ab249b03dbab5849e0f
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-01-10 23:39:50 -0500

    add array exercise add book json exercise

---
+++ module_1/array/arr_exercise.js
@@ -0,0 +1,6 @@
+let randomElements = ["hello", "hi", "hola", "bonjour", "hi_in_simlish"];
+
+randomElements[0] = "goodbye";
+randomElements[4] = "bye_in_simlish";
+
+console.log(randomElements);
---
+++ module_1/book_exercises/book.js
@@ -0,0 +1,13 @@
+const book = {
+    "title": "Invincible Volume 1 (New Edition)",
+    "desc": "Mark Grayson is just like most everyone else his age. Except his father is the most powerful superhero on the planet—Omni-Man. When Mark develops powers of his own, it’s a dream come true. But living up to his father’s legacy is only the beginning of Mark’s problems…",
+    "pages": 208,
+    "author": "Robert Kirkman",
+}
+
+console.log(book.title);
+console.log(book.desc);
+console.log(book.pages);
+console.log(book.author);
+
+console.log(book);
