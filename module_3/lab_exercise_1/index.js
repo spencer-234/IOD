7184b17bdca4b6d2b4ad9c75ce15ee9304182fc1
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-01-24 09:03:08 -0500

    add module_3

---
+++ module_3/lab_exercise_1/index.js
@@ -0,0 +1,19 @@
+const doubleQuotes = "string that can include 'single quotes'";
+
+const singleQuotes = 'string that can include "double quotes"';
+
+const backTicks = `string that can include variables - ${singleQuotes}`;
+
+let age;
+
+// console.log(age);
+
+let three = "3";
+let four = "4";
+let thrity = "30";
+
+let addition = three + four;
+
+let additionTwo = Number(three) + Number(four); 
+
+console.log(additionTwo);
